27b802c80ded9beb46a925948b4a21fa956b520c
马钧恒 <1178966810@qq.com>
2016-11-03 21:22:14 +0800

    Initial Commit

---
+++ iFamily/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iFamily
+//
+//  Created by 马钧恒 on 2016/11/3.
+//  Copyright © 2016年 balancehorse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
